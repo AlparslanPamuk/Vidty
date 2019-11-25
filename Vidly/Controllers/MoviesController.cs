89825cf710ea23c0b15567aab98ef710f7a96153
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-11-25 18:30:50 +0300

    Second Commit
    View Models

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,7 +14,19 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                customers = customers
+            };
+
+            return View(viewModel);
             //return Content("HEllo world");
             //return HttpNotFound();
             //return new EmptyResult();
@@ -25,7 +37,6 @@ namespace Vidly.Controllers
         {
             return Content("id=" + id);
         }
-
         //movies
         public ActionResult Index(int? pageIndex, string sortBy)
         {
@@ -38,6 +49,8 @@ namespace Vidly.Controllers
             return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
